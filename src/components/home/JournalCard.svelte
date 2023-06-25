1906ca3dc453a54cbc8a739b3d8488287c25d2f9
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-06-25 23:57:56 +0100

    update journals section

--- src/components/home/JournalCard.svelte
+++ src/components/home/JournalCard.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
   import type { Journal } from "src/types/journal";
+  import firstNWords from "src/utils/firstNWords";
 
   export let journal: Journal;
 </script>
 
 <div
-  class="flex md:items-center justify-between flex-col md:flex-row hover:border-l-4 py-5 hover:border-primary hover:cursor-pointer hover:px-2 transition-all duration-300 ease-in-out"
+  class="flex md:items-center justify-between flex-col md:flex-row hover:border-l-4 py-5 space-y-5 hover:border-primary hover:cursor-pointer hover:px-2 transition-all duration-300 ease-in-out"
 >
   <div class="max-w-[60rem]">
     <h3 class="text-primary font-bold">
       {journal.title}
     </h3>
     <p>
-      {journal.description}
+      {firstNWords(journal.description, 20)}
     </p>
   </div>
-  <p class="text-[1.5rem]">
+  <p class="font-bold text-[1.5rem]">
     {journal.date}
   </p>
 </div>
--- src/components/home/JournalsSection.svelte
+++ src/components/home/JournalsSection.svelte
@@ -4,35 +4,39 @@
 
   const journals = [
     {
-      title: "Journal of the American Medical Association",
-      link: "https://jamanetwork.com/journals/jama",
+      title: "AHRO Review of Dentistry",
+      link: "https://www.scienceopen.com/collection/AHRO_ReviewDentistry",
       image: "https://www.jamanetwork.com/Content/images/JAMA-logo.png",
-      date: "2021-10-01",
-      description: "The latest medical research findings and review articles.",
+      date: "2022-03-03",
+      description:
+        "international open-access journal that covers all aspects of dental sciences.",
     },
     {
-      title: "New England Journal of Medicine",
-      link: "https://www.nejm.org/",
+      title: "AHRO Review of Pharmaceutics and Pharmacology",
+      link: "https://www.scienceopen.com/collection/AHRO_ReviewsPharmaceutics",
       image:
         "https://www.nejm.org/na101/home/literatum/publisher/mms/journals/content/nejm/0/nejm.logo.png?metadata=false",
-      date: "2021-10-01",
-      description: "The latest medical research findings and review articles.",
+      date: "2022-01-25",
+      description:
+        "international open-access journal that promotes the dissemination of quality knowledge in all aspects of pharmaceutical sciences",
     },
     {
-      title: "The Lancet",
-      link: "https://www.thelancet.com/",
+      title: "AHRO Reviews of Nursing & Midwifery",
+      link: "https://www.scienceopen.com/collection/AHRO_ReviewsNursing",
       image:
         "https://www.thelancet.com/cms/asset/0a0e0f0e-0e0e-4b1e-8b0e-0e0e4b1e8b0e/tla-logo.png",
-      date: "2021-10-01",
-      description: "The latest medical research findings and review articles.",
+      date: "2022-01-25",
+      description:
+        "International  open-access journal that promotes the dissemination of quality knowledge in all aspects of nursing practice.",
     },
     {
-      title: "Nature",
-      link: "https://www.nature.com/",
+      title: "Annals of Tropical Medicine & Public Health",
+      link: "https://www.scienceopen.com/collection/AHRO_ATMPH",
       image:
         "https://media.springernature.com/w92/springer-static/cover/journal/41586.jpg",
-      date: "2021-10-01",
-      description: "The latest medical research findings and review articles.",
+      date: "2021-11-23",
+      description:
+        "The Annals of Tropical Medicine and Public Health (ATMPH) published by AHRO is an Open Access Journal established in 2008 and publishes original scientific articles, with emphasis on clinical and laboratory sciences, clinical & tropical medicine, microbiology, public health, Parasitology, infectious diseases, immunology, epidemiology, basic and molecular biology, virology, mycology, surgical sciences, health management, and biomedical technology",
     },
   ];
 </script>
@@ -42,7 +46,7 @@
     {#if !journals.length}
       <div />
     {:else}
-      <div class="mb-10 flex justify-between items-center">
+      <div class="my-10 flex justify-between items-center">
         <h2 class="text-[3rem] font-extrabold">Our Journals</h2>
         <a href="/blog" class="text-primary-500 hover:underline text-secondary"
           >Explore jounrals</a
