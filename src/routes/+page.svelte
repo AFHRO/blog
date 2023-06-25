9c193c17ff932e7e7ff882ed0a019e2beda4d1cd
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-06-25 21:56:25 +0100

    update footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,16 @@
 <script lang="ts">
-  import GlobalFooter from "src/components/GlobalFooter.svelte";
   import HeroSection from "src/components/home/HeroSection.svelte";
   import JournalsSection from "src/components/home/JournalsSection.svelte";
   import RecentArticlesSection from "src/components/home/RecentArticlesSection.svelte";
-  import { afterNavigate } from "$app/navigation";
 
   export let data: any;
 
   const { recentPosts } = data;
-
-  afterNavigate(() => {
-    document.getElementById("page")?.scrollTo(0, 0);
-  });
-
-  let scrollY: any = 0;
 </script>
 
-<svelte:window bind:scrollY />
-<HeroSection {scrollY} />
+<HeroSection />
 
 <main class="bg-white py-[5rem]">
   <RecentArticlesSection {recentPosts} />
   <JournalsSection />
 </main>
-<GlobalFooter />
