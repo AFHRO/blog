b02469e531de91b5f309becea48f0ea006c8f654
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-06-26 01:30:23 +0100

    nav toggle animation

--- src/components/GlobalHeader.svelte
+++ src/components/GlobalHeader.svelte
@@ -8,6 +8,8 @@
 
   export let toggleMenu: () => void;
 
+  export let showMenu: boolean;
+
   $: linksClasses = hasScrolledDown
     ? " text-primary hover:text-primary hover:underline"
     : " text-white hover:text-primary hover:underline";
@@ -73,25 +75,53 @@
           on:click={toggleMenu}
           data-collapse-toggle="navbar-default"
           type="button"
-          class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 z-[99] lg:hidden"
+          class="w-30 h-30 rounded-lg focus:outline-none z-[99] lg:hidden"
           aria-controls="navbar-default"
           aria-expanded="false"
         >
           <span class="sr-only">Open main menu</span>
-          <svg
-            class="w-6 h-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-            ><path
-              fill-rule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
-            /></svg
-          >
+          <div
+            class="w-[3rem] h-[3rem] nav__toggle-icon"
+            class:--active={showMenu}
+          />
         </button>
       </div>
     </div>
   </div>
 {/if}
+
+<style lang="scss">
+  .nav__toggle-icon {
+    position: relative;
+    &::before,
+    &::after {
+      content: "";
+      display: block;
+      width: 3rem;
+      height: 2px;
+      background-color: #fff;
+      transition: all 0.2s ease-in-out;
+      position: absolute;
+      left: 0;
+      right: 0;
+    }
+
+    &::before {
+      top: 0.4rem;
+    }
+
+    &::after {
+      top: 1.5rem;
+    }
+
+    &.--active {
+      &::before {
+        transform: rotate(45deg) translate3d(3px, 3px, 0);
+      }
+
+      &::after {
+        transform: translate3d(0, -4px, 0) rotate(-45deg);
+      }
+    }
+  }
+</style>
