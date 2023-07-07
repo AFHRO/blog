7c4846ed073b9493b9c7f6e377d2f1ec1902aa1d
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-07-07 15:30:53 +0100

    update about hero

--- src/components/about/HeroSection.svelte
+++ src/components/about/HeroSection.svelte
@@ -1,15 +1,14 @@
-<script>
-  const headingClass =
-    "mb-4 font-extrabold tracking-tight leading-none text-primary md:text-5xl text-4xl lg:text-7xl";
-</script>
-
 <section class="bg-white">
   <div
     class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 min-h-[70vh] flex align-middle justify-center flex-col space-y-5"
   >
-    <h1 class={headingClass}>Improving Global Health through research</h1>
+    <h1
+      class={"mb-4 font-extrabold tracking-tight leading-none text-primary md:text-5xl text-4xl lg:text-8xl"}
+    >
+      Improving Global Health through research
+    </h1>
     <p
-      class="mb-8 text-2xl font-normal text-gray-500 lg:text-3xl sm:px-16 lg:px-48"
+      class="mb-8 text-2xl font-normal text-gray-500 lg:text-4xl sm:px-16 lg:px-48"
     >
       Africa Health Research Organisation (AHRO) is a non-profit award-wining
       organisation that aims to improve global health through research. We are a
