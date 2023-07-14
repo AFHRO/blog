c727497a9c975c7200124d331276fdca801ccd76
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-07-14 15:58:11 +0100

    update subscribe form ui

--- src/components/post/SubscribeCard.svelte
+++ src/components/post/SubscribeCard.svelte
@@ -8,13 +8,13 @@
   >
     <div class="px-2 mt-6">
       <div class="text-center">
-        <h2 class="text-grey-800 font-medium w-full">
+        <h2 class="text-gray-800 font-medium w-full text-6xl mb-5">
           Subsribe to our mailing list
         </h2>
-        <!-- <span class="text-base text-white">
+        <div class="text-md text-gray-800">
           Regular content around health research and publishing straight to your
           inbox
-        </span> -->
+        </div>
         <div class="w-full text-center mt-10">
           <form action="#">
             <div class="mx-auto p-1 pr-0 flex gap-2 justify-center">
@@ -26,7 +26,7 @@
               <button
                 type="submit"
                 class="btn text-white font-semibold bg-gray-800 p-4 rounded-lg hover:bg-gray-900 btn--md min-w-[15rem] inline-block"
-                >Get started</button
+                >Subscribe</button
               >
             </div>
           </form>
