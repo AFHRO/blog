b68643845732f1d0ebdc8ddd89238f09b7c259c0
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-07-15 22:51:18 +0100

    create category page

---
+++ src/routes/blog/category/[category]/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import PostsContainer from "src/components/PostsContainer.svelte";
+  import type { Post } from "src/types/posts";
+  import { afterNavigate } from "$app/navigation";
+  import fetchPosts from "src/utils/fetchPosts";
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+
+  const { category } = data || {};
+
+  $: posts = data?.posts || [];
+
+  // let posts: Post[] = [];
+  let loading = false;
+</script>
+
+{#key posts}
+  <PostsContainer {posts} {loading} currentPage={data.page} {category} />
+{/key}
