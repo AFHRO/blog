7708b9ef65d54bfa1154d1d87301cae144414c42
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-07-18 10:53:26 +0100

    update meta tags order

--- src/components/PageHead.svelte
+++ src/components/PageHead.svelte
@@ -7,34 +7,35 @@
     image?: string;
     url?: string;
     keywords?: string | string[];
+    author?: string;
   }
 
   export let metaContent: MetaContent | undefined = defaultMetaContent;
   metaContent = { ...defaultMetaContent, ...metaContent };
 
-  const { title, description, keywords, image, url } = metaContent;
+  const { title, description, keywords, image, url, author } = metaContent;
 </script>
 
 <svelte:head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width,initial-scale=1" />
   <title>{title}</title>
   <meta name="description" content={description} />
-  <!-- <meta name="keywords" content={keywords} />
-    <meta name="author" content={author} /> -->
-  <meta name="theme-color" content="#0498b4" />
-  <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:site" content="@sveltejs" />
-  <meta name="twitter:title" content={title} />
-  <meta name="twitter:description" content={description} />
-  <meta name="twitter:image" content={image} />
   <meta property="og:type" content="website" />
   <meta property="og:title" content={title} />
   <meta property="og:description" content={description} />
   <meta property="og:image" content={image} />
   <meta property="og:url" content={url} />
-  <title>{title}</title>
+  {#if keywords}
+    {#if typeof keywords === "string"}
+      <meta name="keywords" content={keywords} />
+    {:else}
+      <meta name="keywords" content={keywords.join(", ")} />
+    {/if}
+  {/if}
+  <meta name="author" content={author} />
   <meta name={"description"} content={description} />
   <meta name="twitter:title" content={title} />
   <meta name="twitter:description" content={description} />
+  <meta name="twitter:image" content={image} />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:site" content="@sveltejs" />
 </svelte:head>
