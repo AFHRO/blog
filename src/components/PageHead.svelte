112d06b06b39bb11af4d97be70f15a7fa2f2af90
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-07-18 10:21:51 +0100

    update meta tags

--- src/components/PageHead.svelte
+++ src/components/PageHead.svelte
@@ -37,6 +37,4 @@
   <meta name={"description"} content={description} />
   <meta name="twitter:title" content={title} />
   <meta name="twitter:description" content={description} />
-  <meta property="og:title" content={title} />
-  <meta property="og:description" content={description} />
 </svelte:head>
